6e6c58d1c9635bb1afe541705a9e7acc45641d5b
Nick Hand <nicholas.adam.hand@gmail.com>
2013-02-02 18:35:55 -0800

    added a weighted number generator

--- utils/random_sampling.py
+++ utils/random_sampling.py
@@ -86,3 +86,19 @@ def weighted_choice(weights):
     for i, total in enumerate(totals):
         if rnd < total:
             return i
+            
+class WeightedRandomGenerator(object):
+    def __init__(self, weights):
+        self.totals = []
+        running_total = 0
+
+        for w in weights:
+            running_total += w
+            self.totals.append(running_total)
+
+    def next(self):
+        rnd = random.random() * self.totals[-1]
+        return bisect.bisect_right(self.totals, rnd)
+
+    def __call__(self):
+        return self.next()
