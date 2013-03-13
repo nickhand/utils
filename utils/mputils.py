6cecd11dc42d2060904f83603ca2b2e56bc56a10
Nick Hand <nicholas.adam.hand@gmail.com>
2013-03-13 14:43:47 -0700

    attempting to catch keyboard interrupts

--- utils/mputils.py
+++ utils/mputils.py
@@ -125,16 +125,22 @@ class mp_master(object):
         @brief start the workers and do the work
         """
         
-        # start the work
-        for w in self.workers:
-            w.start()
+        try: 
+            # start the work
+            for w in self.workers:
+                w.start()
             
-        # add a poison pill for each worker
-        for i in range(len(self.workers)):
-            self.tasks.put(None)
+            # add a poison pill for each worker
+            for i in range(len(self.workers)):
+                self.tasks.put(None)
             
-        # wait for all tasks to finish
-        self.tasks.join()
+            # wait for all tasks to finish
+            self.tasks.join()
+        except KeyboardInterrupt:
+            print "caught keyboard interrupt.."
+            for w in self.workers:
+                w.terminate()
+            
         
         return
         
