9e218a1f28d06af12a3b21c20d642d5fcab9de4a
Nick Hand <nicholas.adam.hand@gmail.com>
2013-03-13 17:12:07 -0700

    trying to solve ctrl-c

--- utils/mputils.py
+++ utils/mputils.py
@@ -147,6 +147,7 @@ class mp_master(object):
             print "caught keyboard interrupt.."
             for w in self.workers:
                print "PID", w.pid
+               os.waitpid(w.pid)
                w.terminate()
                w.join()
             
