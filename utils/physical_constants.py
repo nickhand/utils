c8d07232dd72aa73f2b591c5d609078a8bcf8810
Nick Hand <nicholas.adam.hand@gmail.com>
2013-04-15 20:20:57 -0700

    updated some constants

--- utils/physical_constants.py
+++ utils/physical_constants.py
@@ -1,9 +1,11 @@
-# 
-#  physical_constants.py
-#  <constants assuming cgs units>
-#  
-#  Created by Nick Hand on 2012-11-03.
-# 
+"""
+ physical_constants.py
+ 
+ author: Nick Hand
+ contact: nhand@berkeley.edu
+ creation date: 04/10/2012
+"""
+
 from numpy import pi
 
 # set up cgs units
@@ -25,22 +27,22 @@ nano  = 1e-9
 pico  = 1e-12
 
 # fundamental constants
-h_planck   = 6.62606876e-27   # planck constant
-h_bar      = h_planck/(2.*pi) # reduced planck constant
-c_light    = 2.99792458e10    # speed of light
-k_b        = 1.3806503e-16    # Boltzmann constant
-m_p        = 1.67262158e-24   # proton mass
-m_e        = 9.10938188e-28   # electron mass
-q_e        = 4.803242e-10     # e.s.u.
-G          = 6.673e-8         # Newton's constant
-eV         = 1.60217653e-12   # ergs
-N_a        = 6.0221e23        # Avogadro's constant
-a_rad      = 7.5646e-15       # radiation constant
-sigma_sb   = 5.6705e-5        # Stefan-Boltzmann constant
-sigma_T    = 6.6524e-25       # Thomson cross section
-T_cmb      = 2.72528          # temperature of the CMB
-H_0        = 100.0            # in units of h km/s/Mpc
-a_0        = 5.292e-9         # Bohr radius
+h_planck = 6.62606957e-27    # planck constant
+h_bar    = h_planck/(2.*pi) # reduced planck constant
+c_light  = 2.99792458e10    # speed of light
+k_b      = 1.3806488e-16    # Boltzmann constant
+m_p      = 1.672621777e-24   # proton mass
+m_e      = 9.10938291e-28   # electron mass
+q_e      = 4.80320425e-10     # e.s.u.
+G        = 6.67384e-8        # Newton's constant
+eV       = 1.60217657e-12   # ergs
+N_a      = 6.02214129e23        # Avogadro's constant
+sigma_sb = 5.670373e-5        # Stefan-Boltzmann constant
+a_rad    = 4*sigma_sb/c_light       # radiation constant
+sigma_T  = 6.652458734e-25       # Thomson cross section
+T_cmb    = 2.72528          # temperature of the CMB
+H_0      = 100.0            # in units of h km/s/Mpc
+a_0      = 5.2917721092e-9         # Bohr radius
 
 # length conversion factors
 km       = 1.0e+5 * cm  
@@ -55,10 +57,10 @@ barn     = 1e-24  * cm**2
 # energy and power conversion factors
 joule           = 1e7 * erg
 watt            = 1e7 * erg / second
-rydberg         = 10973731.568 * meter**(-1.)
+rydberg         = 10973731.568539 * meter**(-1.) * h_planck*c_light
 
 # astronomical constants
-au         = 499.004782 * second * c_light  # the astronomical unit
+au         = 149597870700*meter      # the astronomical unit
 degree    = pi/180.
 arcminute = degree/60.
 arcsecond = arcminute/60.               # radians
