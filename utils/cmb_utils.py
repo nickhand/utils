95cd61393535c7c3f703b265a8b329c95b0a7041
Nick Hand <nicholas.adam.hand@gmail.com>
2013-06-19 13:17:13 -0700

    added better comments to cmb_utils.py

--- utils/cmb_utils.py
+++ utils/cmb_utils.py
@@ -1,83 +1,134 @@
+"""
+ cmb_utils.py
+ utility functions useful for CMB analysis
+ 
+ author: Nick Hand
+ contact: nhand@berkeley.edu
+"""
 import numpy as np
 from physical_constants import *
 
-def planck_law(nu, T = T_cmb):
+def planck_law(nu, T=T_cmb):
     """
-    @brief Planck's law
-    @param nu: frequency in GHz
-    @keyword T: the temperature to use in Kelvin
-    @return specific intensity in MJy/sr
+    Planck's law, B_nu
+    
+    Parameters
+    ----------
+    nu : float or numpy.ndarray 
+        the frequency in GHz
+    T : float, optional 
+        the temperature to evaluate Planck's law at in Kelvin. Default is T_cmb.
+        
+    Returns
+    -------
+    B_nu : float or numpy.ndarray
+        specific intensity in MJy/sr
     """
-    # the dimensionaless frequency parameter
     x = h_planck*giga*nu / (k_b*T)
-    
-    # intensity in erg/s/cm/cm/Hz
     b_nu = (2*h_planck*((giga*nu)**3) / c_light**2) * 1/(np.exp(x)- 1.)
-    
-    # return in MJy/sr
     return  b_nu / (mega*jansky)
+#end planck_law
 
-
-def rayleigh_jeans_law(nu, T = T_cmb):
+#-------------------------------------------------------------------------------
+def rayleigh_jeans_law(nu, T=T_cmb):
     """
-    @brief Rayleigh Jeans limit of Planck's law for h*nu << kT
-    @param nu: frequency in GHz
-    @keyword T: the temperature to use in Kelvin
-    @return intensity in MJy/sr
-    """
+    The Rayleigh Jeans limit of Planck's law for h*nu << kT
     
-    # intensity in erg/s/cm/cm/Hz
+    Parameters
+    ----------
+    nu : float or numpy.ndarray 
+        the frequency in GHz
+    T : float, optional 
+        the temperature to evaluate Planck's law at in Kelvin. Default is T_cmb.
+        
+    Returns
+    -------
+    B_nu : float or numpy.ndarray
+        specific intensity in MJy/sr
+    """
     b_nu = 2. * (giga*nu)**2 * k_b * T / c_light**2
-    
-    # return in MJy/sr
     return b_nu / (mega*jansky)
+#end rayleigh_jeans_law
 
-
-def dBdT(nu, T = T_cmb):
+#-------------------------------------------------------------------------------
+def dBdT(nu, T=T_cmb):
     """
-    @brief derivative of Planck's law with respect to temperature
-    @param nu: frequency in GHz
-    @keyword T: the temperature to use in Kelvin
-    @return value of derivative in cgs units
+    The derivative of Planck's law with respect to temperature.
+    
+    Parameters
+    ----------
+    nu : float or numpy.ndarray 
+        the frequency in GHz
+    T : float, optional 
+        the temperature to evaluate Planck's law at in Kelvin. Default is T_cmb.
+    
+    Returns
+    -------
+    deriv : float or numpy.ndarray
+        the value of derivative in cgs units
     """
-    # the constant out front
     A = 2 * h_planck**2 * (giga*nu)**4 / k_b  / (c_light*T)**2
-    
-    # dimensionaless freq param
     x = h_planck*giga*nu / k_b / T
-    
-    # return the derivative
     return  A * np.exp(x) / (np.exp(x) - 1.)**2
-    
+#end dBdT
 
+#-------------------------------------------------------------------------------
 def dTdB(nu, T = T_cmb):
     """
-    @brief inverse of the derivative of Planck's law with respect to temperature
-    @param nu: frequency in GHz
-    @keyword T: the temperature to use in Kelvin
-    @return value of derivative in cgs units
+    The inverse of the derivative of Planck's law with respect to temperature.
+    
+    Parameters
+    ----------
+    nu : float or numpy.ndarray 
+        the frequency in GHz
+    T : float, optional 
+        the temperature to evaluate Planck's law at in Kelvin. Default is T_cmb.
+    
+    Returns
+    -------
+    deriv : float or numpy.ndarray
+        the value of derivative in cgs units
     """
 
     return dB_dT(nu, T)**(-1.)
+#end dTdB
 
+#-------------------------------------------------------------------------------
 def dBdT_RJ(nu):
     """
-    @brief derivative of the Rayleigh Jeans approximation with respect to temperature
-    @param nu: frequency in GHz
-    @keyword T: the temperature to use in Kelvin
-    @return value of derivative in cgs units
+    The derivative of the Rayleigh Jeans approximation with respect to 
+    temperature, which is independent of temperature.
+    
+    Parameters
+    ----------
+    nu : float or numpy.ndarray 
+        the frequency in GHz
+    T : float, optional 
+        the temperature to evaluate Planck's law at in Kelvin. Default is T_cmb.
+    
+    Returns
+    -------
+    deriv : float or numpy.ndarray
+        the value of derivative in cgs units
     """
-
     return 2. * k_b * (giga*nu)**2 / c_light**2
+#end dBdT_RJ
 
-
+#-------------------------------------------------------------------------------
 def centroid(mp):
     """
-    @brief compute the centroid of an input map, weighted by the data
-    @param mp: the map object (flipper.flipper.liteMap)
-    @return value of centroid in degrees
-    """
+    Compute the centroid of an input map, weighted by the data
     
+    Parameters
+    ----------
+    mp : flipper.liteMap
+        the input liteMap object storing the data
+    
+    Returns
+    -------
+    cmdeg : tuple
+        the value of the centroid (ra, dec) in degrees
+    """
     data = mp.data
     Nx = mp.Nx
     Ny = mp.Ny
@@ -105,16 +156,20 @@ def centroid(mp):
     cmdeg = mp.pixToSky(xcm, ycm)
 
     return cmdeg
+#end centroid
 
-
+#-------------------------------------------------------------------------------
 def f_sz(nu):
     """
-    @brief the frequency dependence of the thermal SZ effect
-    @param nu: frequency in GHz
-    @return value of frequency function
+    The frequency dependence of the thermal SZ effect
+    
+    Parameters
+    ----------
+    nu : float or numpy.ndarray 
+        the frequency in GHz
     """
-
     x = h_planck*giga*nu / k_b / T_cmb
-    f = x*(np.exp(x) + 1.) / (np.exp(x) - 1.) - 4.0
+    return x*(np.exp(x) + 1.) / (np.exp(x) - 1.) - 4.0
+#end f_sz
 
-    return f
+#-------------------------------------------------------------------------------
